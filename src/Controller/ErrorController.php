61084b5c2c9192c88a83e22aff6e5530580b1cdc
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-11-16 11:34:06 -0600

    Modificacion (CSS)

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -13,7 +13,9 @@ class ErrorController extends AbstractController
     {
         return $this->render('error/index.html.twig', [
             'page_error' => 'ERROR: [404]',
-            'context' => 'Esta pagina no se encuentra disponible en estos momentos. Se ha encontrado fallas en la ruta, revise su guia correcta.',
+            'context' => 'Esta pagina no se encuentra disponible en estos momentos. Se ha encontrado fallas en la ruta. Revise su guia correcta o regrese al:',
         ]);
     }
 }
+
+#https://symfony.com/doc/current/controller/error_pages.html
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,37 +8,46 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page/{id}', name: 'app_page')]
+    /*#[Route('/page/{id}', name: 'app_page')]
     public function index(string $id): Response
     {
         if (ctype_digit($id)){
             $id = (int)$id;
             return $this->render('page/index.html.twig', [
-                'page' => 'página: ' .$id,
+                'pag' => 'página: ' .$id,
             ]);
         }else {
             return $this->redirectToRoute('app_error');
         } 
-    }
+    }*/
 
     #[Route('/page/10', name: 'app_photo1')]
     public function photo1(): Response
     {
-            return $this->render('page/index.html.twig', [
+            return $this->render('page/pag1.html.twig', [
+                'pag' => 'página: 10',
             ]);
     }
 
     #[Route('/page/20', name: 'app_photo2')]
     public function photo2(): Response
     {
-            return $this->render('page/index.html.twig', [
+            return $this->render('page/pag2.html.twig', [
+                'pag' => 'página: 20',
             ]);
     }
 
     #[Route('/page/30', name: 'app_photo3')]
     public function photo3(): Response
     {
-            return $this->render('page/index.html.twig', [
+            return $this->render('page/pag3.html.twig', [
+                'pag' => 'página: 30',
             ]);
+    }
+
+     #[Route('/page/{id}', name: 'app_edOx')]
+    public function error(string $id): Response
+    {
+            return $this->redirectToRoute('app_error');
     }
 }
