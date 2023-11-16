3a385b89c605678b65fa927c4b2bed024914b1bc
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-11-16 13:42:53 -0600

    PAckages

---
+++ src/Serializer/MyCustomProblemNormalizer.php
@@ -0,0 +1,23 @@
+namespace App\Serializer;
+
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class MyCustomProblemNormalizer implements NormalizerInterface
+{
+    public function normalize($exception, string $format = null, array $context = []): array
+    {
+        return [
+            'content' => 'This is my custom problem normalizer.',
+            'exception'=> [
+                'message' => $exception->getMessage(),
+                'code' => $exception->getStatusCode(),
+            ],
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = []): bool
+    {
+        return $data instanceof FlattenException;
+    }
+}
